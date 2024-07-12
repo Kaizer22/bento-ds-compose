05ab2d04bd6d67d64f9d8726b29495cf4a51a4bc
Denis Sh <dshebut@rambler.ru>
2024-07-12 12:24:13 +0300

    Switch

--- app/src/main/java/com/desh2403/bento_ds_compose/playground/PlaygroundActivity.kt
+++ app/src/main/java/com/desh2403/bento_ds_compose/playground/PlaygroundActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.desh2403.bento_ds_compose.uikit.component.input.BentoDSSwitch
 import com.desh2403.bento_ds_compose.uikit.component.loading.AreaLoader
 import com.desh2403.bento_ds_compose.uikit.component.loading.InlineLoader
 import com.desh2403.bento_ds_compose.uikit.theme.BentoDSTheme
@@ -17,15 +24,23 @@ class PlaygroundActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BentoDSTheme {
-                Scaffold { paddingValues ->
+                Scaffold(
+                    modifier = Modifier.safeDrawingPadding(),
+                ) { paddingValues ->
                     AreaLoader(
                         modifier = Modifier.padding(paddingValues),
-                        isLoading = true,
+                        isLoading = false,
                         text = "Loading..."
                     ) {
-                        InlineLoader(
-                            text = "Loading..."
-                        )
+                        Column {
+                            InlineLoader(
+                                text = "Loading..."
+                            )
+                            var c by remember { mutableStateOf(true) }
+                            BentoDSSwitch(checked = c) {
+                                c = it
+                            }
+                        }
                     }
                 }
             }
--- uikit/src/main/java/com/desh2403/bento_ds_compose/uikit/component/input/Switch.kt
+++ uikit/src/main/java/com/desh2403/bento_ds_compose/uikit/component/input/Switch.kt
@@ -1,9 +1,56 @@
 package com.desh2403.bento_ds_compose.uikit.component.input
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.desh2403.bento_ds_compose.uikit.theme.BentoDSTheme
 
 // TODO Switch
 @Composable
-fun Switch() {
+fun BentoDSSwitch(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    enabled: Boolean = true,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Switch(
+        modifier = modifier,
+        checked = checked,
+        enabled = enabled,
+        onCheckedChange = onCheckedChange,
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = BentoDSTheme.colors.prSolidEnabledFg,
+            checkedTrackColor = BentoDSTheme.colors.prSolidEnabledBg,
+            checkedBorderColor = BentoDSTheme.colors.prSolidEnabledBg,
+            //checkedIconColor =,
+            uncheckedThumbColor = BentoDSTheme.colors.fgInteractive,
+            uncheckedTrackColor = BentoDSTheme.colors.bgPrimary,
+            uncheckedBorderColor = BentoDSTheme.colors.outlineInputEnabled,
+            //uncheckedIconColor =,
+            disabledCheckedThumbColor = BentoDSTheme.colors.disSolidFg,
+            disabledCheckedTrackColor = BentoDSTheme.colors.disSolidBg,
+            disabledCheckedBorderColor = BentoDSTheme.colors.disSolidBg,
+            //disabledCheckedIconColor =,
+            disabledUncheckedThumbColor =BentoDSTheme.colors.fgDisabled,
+            disabledUncheckedTrackColor = BentoDSTheme.colors.bgPrimary,
+            disabledUncheckedBorderColor = BentoDSTheme.colors.fgDisabled,
+            //disabledUncheckedIconColor =,
+        )
+    )
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SwitchPreview() {
+    BentoDSTheme {
+        Column {
+            BentoDSSwitch(checked = true) {}
+            BentoDSSwitch(checked = false) {}
+            BentoDSSwitch(checked = true, enabled = false) {}
+            BentoDSSwitch(checked = false, enabled = false) {}
+        }
+    }
 }
